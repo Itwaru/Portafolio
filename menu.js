af70551220a3de9af7535bb0c32c006216026642
Edward Godoy <enriqueitwaru@gmail.com>
2022-04-15 13:21:00 -0500

    Actualizacion

--- menu.js
+++ menu.js
@@ -4,12 +4,13 @@ export default function hamburger(panelBtn,panel,link) {
         if (e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
             document.querySelector(panel).classList.toggle("is-active")
             document.querySelector(panelBtn).classList.toggle("is-active")
+            
         }
         if (e.target.matches(link)) {
             document.querySelector(panel).classList.remove("is-active")
             document.querySelector(panelBtn).classList.remove("is-active")
-            
         }
     })
+  
 
 }
