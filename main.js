932b2924b156e2d8fba66a4c522eda3fe6962108
Itwaru <86633540+Itwaru@users.noreply.github.com>
2022-04-15 14:34:31 +0000

    Deploying to gh-pages from @ Itwaru/Portafolio@77bac253964c1547f4e916faa7a5062dd98a59e5 🚀

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import fade from "./fade.js";
 import form from "./formulario.js";
 import hamburger from "./menu.js";
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", (e)=>{
-
-hamburger(".panel-btn",".panel",".panel_enlaces");
 form();
 fade()
+hamburger(".panel-btn",".panel",".panel_enlaces");
 
-})
+
+
+/* document.addEventListener("DOMContentLoaded", (e)=>{
+
+
+
+
+}) */
